42a27c12f101d8b13f73530c75d15d7c541aac38
Hiroya-W <hyuyukun@gmail.com>
2022-11-16 18:03:08 +0900

    :sparkles: Show informations

--- yuyufetch.go
+++ yuyufetch.go
@@ -34,10 +34,30 @@ func Main(args []string) int {
 }
 
 func (cli *CLI) Run(args []string) error {
-    lines := strings.Split(yuyu_logo, "\n")
+	logo_lines := strings.Split(Yuyu_logo, "\n")
+	yuyu_info := YuyuInfo.GetStyledContents()
 
-    for _, line := range lines {
-        fmt.Fprintf(cli.Stdout, "%s\n", line)
-    }
-    return nil
+	var max_len int
+	if len(logo_lines) > len(yuyu_info) {
+		max_len = len(logo_lines)
+	} else {
+		max_len = len(yuyu_info)
+	}
+
+	for i := 0; i < max_len; i++ {
+		if i < len(logo_lines) {
+			fmt.Fprintf(cli.Stdout, "%s", logo_lines[i])
+		} else {
+			fmt.Fprintf(cli.Stdout, "%s", strings.Repeat(" ", len(logo_lines[0])))
+		}
+
+		fmt.Fprint(cli.Stdout, "   ")
+
+		if i < len(yuyu_info) {
+			fmt.Fprintf(cli.Stdout, "%s", yuyu_info[i])
+		}
+		fmt.Fprintln(cli.Stdout, "")
+	}
+
+	return nil
 }
